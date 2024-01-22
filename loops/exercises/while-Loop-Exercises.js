6e36b3fe4ef1421dfa9d5c914f54e82a24b593dd
ashasingh04 <asha.sgsits@gmail.com>
2024-01-21 20:20:17 -0600

    Class 4 Exercises: Repeating With Loops

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,35 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
-
+const input = require("readline-sync");
+let fuelLevel = 0;
+let noOfAstronauts=0;
+let altitude=0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-
-
-
-
-
+while(fuelLevel <= 5000 || fuelLevel >30000 || isNaN(fuelLevel)){
+    fuelLevel = input.question("Please enter starting fuel level : ");
+}
+console.log('-----------------------------------------------------');
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
-  
-  
-  
+while(noOfAstronauts < 1 || noOfAstronauts > 7 || isNaN(noOfAstronauts)){
+    noOfAstronauts = input.question("Please enter the number of astronauts : ");
+ } 
+ console.log('-----------------------------------------------------');  
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
-
+while(fuelLevel-100*noOfAstronauts >= 0){
+  altitude += 50;
+  fuelLevel -= 100 * noOfAstronauts;
+  // console.log(`altitude : ${altitude}`);
+  // console.log(`fuelLevel : ${fuelLevel}`);
+}
+console.log('-----------------------------------------------------');
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+let output = `The shuttle gained an altitude of ${altitude} km. `;
+if (altitude >= 2000){
+  output += "Orbit achieved!";
+} else{
+  output += "Faile to reach orbit.";
+}
+console.log(output);
