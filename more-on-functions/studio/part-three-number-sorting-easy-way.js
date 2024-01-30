5dbe82a566b7406d9449604d44bc90b8545e0ce5
ashasingh04 <asha.sgsits@gmail.com>
2024-01-29 20:33:09 -0600

    Class 6 Studio: More on Functions completed.

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -2,7 +2,21 @@
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+let numArray = [nums1, nums2, nums3];
 
 //Sort each array in ascending order.
+// console.log(nums1.sort(function(a, b){return a-b}));
+// console.log(nums2.sort(function(a, b){return a-b}));
+// console.log(nums3.sort(function(a, b){return a-b}));
+
+numArray.forEach((num) => { num.sort((a, b) => {return a - b})});
+console.log(numArray);
 
 //Sort each array in decending order.
+// console.log(nums1.sort(function(a, b){return b-a}));
+// console.log(nums2.sort(function(a, b){return b-a}));
+// console.log(nums3.sort(function(a, b){return b-a}));
+numArray.forEach((num) => { num.sort((a, b) => {return b - a})});
+console.log(numArray);
+// console.log(nums1);
+// console.log("Avove functions alter the origional arrays");
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -19,11 +19,51 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
-
-/* BONUS MISSION: Refactor your sorting function to use recursion below:
- */
-
+function sortArray(arr)
+{
+  let sortedArray = [];
+  let minValue;
+  while(arr.length > 0)
+  {
+    minValue = findMinValue(arr);
+    sortedArray.push(minValue);
+    arr.splice(arr.indexOf(minValue),1);
+  }
+  return sortedArray;
+}
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(sortArray(nums1));
+console.log(sortArray(nums2));
+console.log(sortArray(nums3));
+
+
+
+/* BONUS MISSION: Refactor your sorting function to use recursion below:
+ */
+function sortArrayRecursion(arr,sortedArr = [])
+{
+  let minValue;
+  if(arr.length <= 0)
+  {
+    return sortedArr;
+  }else
+  {
+    minValue = findMinValue(arr);
+    sortedArr.push(minValue);
+    arr.splice(arr.indexOf(minValue),1);
+    //  recursion function
+    return sortArrayRecursion(arr,sortedArr);
+  }  
+}
+//Sample arrays for testing:
+nums1 = [5, 10, 2, 42];
+nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+let sortArr;
+console.log(sortArrayRecursion(nums1,sortArr));
+console.log(sortArrayRecursion(nums2,sortArr));
+console.log(sortArrayRecursion(nums3,sortArr));
