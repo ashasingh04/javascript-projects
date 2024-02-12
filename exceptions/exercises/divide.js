3bf4ae9ec54dca26edb6396b26753f2a5981e308
ashasingh04 <asha.sgsits@gmail.com>
2024-02-12 13:44:43 -0600

    Exceptions exercise completed.

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,13 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+function divide(numerator,denominator){
+    let result;
+    if(denominator === 0){
+        throw Error("Attempted to divide by zero.");
+    }
+    result = numerator / denominator;
+    return result;
+}
+console.log(divide(2,0));
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,16 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let result;
+    try{
+      result = lab.runLab(3);      
+    } catch(TypeError){
+      console.log("TypeError: lab.runLab is not a function");
+      result = "Error thrown";
+      //console.log("Error thrown");
+    } finally {
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    }   
   }
 }
 
@@ -22,3 +30,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+// Checking: what happens if a student named their function incorrectly?
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
