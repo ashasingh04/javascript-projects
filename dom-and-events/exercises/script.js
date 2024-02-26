29e33e54029038042562cdd6978d51f592a07fb1
ashasingh04 <asha.sgsits@gmail.com>
2024-02-26 12:26:07 -0600

    DOM and events exercise completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function(event) {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+    // mouseover event for "Abort Mission" button
+    missionAbort.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    // Click event for "Abort Mission" button
+    missionAbort.addEventListener("click", function() {
+        let confirm = window.confirm("Are you sure you want to abort the mission?");
+        if (confirm){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    });
 }
 
 window.addEventListener("load", init);
