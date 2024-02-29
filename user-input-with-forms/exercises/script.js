49ce959adabd92b2afa6b45f3cd0911efb3d3af3
ashasingh04 <asha.sgsits@gmail.com>
2024-02-29 16:15:09 -0600

    Forms exercise completed

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,17 @@
 //Code Your Solution Below
+window.addEventListener("load", function () {   
+    let form = this.document.querySelector("form");
+    form.addEventListener("submit", function(event){
+        let testName = document.querySelector("input[name=testName]");
+        let testDate = document.querySelector("input[name=testDate]");
+        let rocketType = document.querySelector("select[name=rocketType]");
+        let boosterCount = document.querySelector("input[name=boosterCount]");
+        let windRating = document.querySelector("input[name=windRating]");
+        let productionServers = document.querySelector("input[name=productionServers]");
+        
+        if(testName.value === "" || testDate.value === "" || rocketType.value === "" || boosterCount.value === "" || windRating.value === "" || productionServers.value === ""){
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+    });
+});
